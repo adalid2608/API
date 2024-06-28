b0dcf9883d6ca2e119152456f0c33606b8c9daed
adalid2608 <adalid.islas26@gmail.com>
2024-06-27 22:51:11 -0600

    Instalación de morgan para ver cada una de las peticiones que se hacen al servidor

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const config = require('./config');
+const morgan = require('morgan')
 
 const app = express();
+const error = require('./red/errors')
 const clients = require('./modules/clients/routes')
+
+//Middleware
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 //Configuración de la variable app
 app.set('port', config.app.port);
@@ -10,5 +17,6 @@ app.set('port', config.app.port);
 
 //Rutas
 app.use('/api/clients', clients)
+app.use(error)
 
 module.exports = app;
